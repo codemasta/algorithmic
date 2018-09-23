3a3f46fb5128c85cdd59384b8e0816cb81c2a9d2
teivah <teivah.io@gmail.com>
2018-09-23 19:12:38 +0200

    Problems

--- src/test/java/io/teivah/arrays/nobleint/Tests.java
+++ src/test/java/io/teivah/arrays/nobleint/Tests.java
@@ -8,6 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Tests {
 	@Test
 	public void test1() {
-		assertEquals(1, new Solution().solve(toArrayList(-4, -2, 0, -1, -6)));
+		assertEquals(1, new Solution().solve(toArrayList(2, 6, 1, 3)));
+	}
+
+	@Test
+	public void test2() {
+		assertEquals(-1, new Solution().solve(toArrayList(1, 2, 2, 3)));
 	}
 }
